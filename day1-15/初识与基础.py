f7ae08865c84d327b4901a72f0f64687b6858765
Leokiko <o2239@qq.com>
2023-03-01 18:33:12 +0800

    Create 初识与基础.py

---
+++ day1-15/初识与基础.py
@@ -0,0 +1,37 @@
+# # 使用交互式代码来进行Python的正确成功安装检测,也就是代替cmd的python --version
+# import sys
+
+# print(sys.version_info)
+# print("\n")
+# print(sys.version)
+
+
+# # 第一个程序代码hello world
+# #这两句代码的输出是一样的
+# print("hello,world!")
+# print("hello", "world", sep=",", end="!\n")
+
+
+# # 行注释
+# """
+# 块注释
+# """
+
+
+# 学习使用turtle在屏幕上绘制图形
+# import turtle
+
+# turtle.pensize(4)
+# turtle.pencolor("red")
+# turtle.forward(100)
+# turtle.right(90)
+# turtle.forward(100)
+# turtle.right(90)
+# turtle.forward(100)
+# turtle.right(90)
+# turtle.forward(100)
+# turtle.mainloop()
+
+
+# #这个是一个import的示例导入，用来检测导入功能是否正常，直接import即可
+# import this
