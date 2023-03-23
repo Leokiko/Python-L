8aecfda5fa247a5c77a6684e15522e177193a3d9
Leokiko <o2239@qq.com>
2023-03-23 09:59:41 +0800

    Update test - 副本 (11).py

--- day21-30/test - 副本 (11).py
+++ day21-30/test - 副本 (11).py
@@ -22,3 +22,10 @@ print(str.islower())
 print(str.isupper())
 print(str.istitle())
 print(str.isspace())
+
+
+# 字符串的相关转换操作
+print(str.upper())  # 把所有字符中的小写字母转换成大写字母
+print(str.lower())  # 把所有字符中的大写字母转换成小写字母
+print(str.capitalize())  # 把第一个字母转化为大写字母，其余小写
+print(str.title())  # 把每个单词的第一个字母转化为大写，其余小写
